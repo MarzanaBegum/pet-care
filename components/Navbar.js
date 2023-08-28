1d594569b63c0071e0ea39e8a09d209fb03b5aeb
MarzanaBegum <marzanaBegum473@gmail.com>
2023-08-28 21:42:18 +0600

    some change

--- components/Navbar.js
+++ components/Navbar.js
@@ -21,8 +21,8 @@ export default function Navbar() {
       {({ open }) => (
         <>
           <div className="px-2 mx-auto bg-white border-b max-w-[1920px] border-gray-50 sm:px-6 lg:px-8">
-            <div className="relative flex items-center justify-between h-16 mx-0 md:mx-20">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 text-orange-500 rounded-md hover:bg-orange-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -35,13 +35,13 @@ export default function Navbar() {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
+              <div className="flex items-center justify-center flex-1 md:items-stretch md:justify-start">
                 <div className="flex items-center flex-shrink-0">
                   <h2 className="text-xl font-semibold cursor-pointer">
                     Pet<span className="text-orange-500">Care</span>
                   </h2>
                 </div>
-                <div className="hidden sm:block sm:ml-6 md:ml-50">
+                <div className="hidden md:block sm:ml-6 md:ml-50">
                   <div className="flex space-x-4">
                     {Navigation.map((item) => (
                       <a
@@ -68,7 +68,7 @@ export default function Navbar() {
               </div>
             </div>
           </div>
-          <Disclosure.Panel className="sm:hidden">
+          <Disclosure.Panel className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {Navigation.map((item) => (
                 <Disclosure.Button
